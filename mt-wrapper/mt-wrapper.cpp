5abe2752922c6c6c0d9c413ae63c298dcc40e4e9
Alexander Bilyak <abilyak@astim.it>
2018-02-20 12:01:31 +0100

    Fix finding mt-orig.exe

--- mt-wrapper/mt-wrapper.cpp
+++ mt-wrapper/mt-wrapper.cpp
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <process.h>
 
-BOOL FileExists(LPCTSTR szPath)
+BOOL FileExists(LPCWSTR szPath)
 {
-    DWORD dwAttrib = GetFileAttributes(szPath);
+    DWORD dwAttrib = GetFileAttributesW(szPath);
 
     return (dwAttrib != INVALID_FILE_ATTRIBUTES &&
         !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
@@ -14,14 +14,20 @@ int __cdecl wmain(int argc, WCHAR **argv, WCHAR **env)
 {
     int tries = 0;
     
+    // Get absolute path of wrapper
+    WCHAR path[1024] = { '\0' };
+    WCHAR origpath[2014] = { '\0' };
+    GetModuleFileNameW(NULL, origpath, MAX_PATH + 1);
+    wcsncat_s(path, origpath, wcsrchr(origpath, '\\') - origpath);
+    wcscat_s(path, L"\\mt-orig.exe");
+
     // Check if we can find mt-orig.exe
-    if (!FileExists("mt-orig.exe"))
+    if (!FileExists(path))
     {
         fprintf(stderr, "Error: mt-orig.exe not found!\n\n");
         return 1;
     }
-    // Get it full path, escape it with qoutes and pass to argv[0]
-    WCHAR* path = _wfullpath(NULL, L"mt-orig.exe", 1022);
+    // Escape full path with qoutes and pass to argv[0]
     WCHAR argv0[1024] = {'\0'};
     wcscpy_s(argv0, L"\"");
     wcscat_s(argv0, path);
